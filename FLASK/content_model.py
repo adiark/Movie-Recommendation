85bec51ae0c7adf19afcefb938425b4ec8d10fcc
Aditya Jain <112529039+adiark@users.noreply.github.com>
2022-12-28 23:09:40 -0500

    Add files via upload

---
+++ FLASK/content_model.py
@@ -0,0 +1,42 @@
+
+
+import pandas as pd
+from math import sqrt
+import numpy as np
+
+movies_df = pd.read_csv('movies.csv')
+ratings_df = pd.read_csv('ratings.csv')
+movies_df['year'] = movies_df.title.str.extract('(\(\d\d\d\d\))',expand=False)
+movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)',expand=False)
+movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '',regex=True)
+movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
+movies_df['genres'] = movies_df.genres.str.split('|')
+movies_df=movies_df[movies_df['year'].notna()]
+movies_df['year']=movies_df['year'].astype('int')
+movies_df=movies_df.loc[movies_df.year>1980]
+moviesWithGenres_df = movies_df.copy()
+
+
+for index, row in movies_df.iterrows():
+    for genre in row['genres']:
+        moviesWithGenres_df.at[index, genre] = 1
+
+moviesWithGenres_df = moviesWithGenres_df.fillna(0)
+
+ratings_df = ratings_df.drop('timestamp',axis= 1)
+
+def recommend(userInput):
+	inputMovies = pd.DataFrame(userInput)
+	inputId = movies_df[movies_df['title'].isin(inputMovies['title'].tolist())]
+	inputMovies = pd.merge(inputId, inputMovies)
+	inputMovies = inputMovies.drop('genres',axis= 1).drop('year',axis= 1)
+	userMovies = moviesWithGenres_df[moviesWithGenres_df['movieId'].isin(inputMovies['movieId'].tolist())]
+	userMovies = userMovies.reset_index(drop=True)
+	userGenreTable = userMovies.drop('movieId',axis= 1).drop('title',axis= 1).drop('genres',axis= 1).drop('year',axis= 1)
+	userProfile = userGenreTable.transpose().dot(inputMovies['rating'])
+	genreTable = moviesWithGenres_df.set_index(moviesWithGenres_df['movieId'])
+	genreTable=genreTable.loc[~genreTable.movieId.isin(userMovies.movieId)]
+	genreTable = genreTable.drop('movieId',axis= 1).drop('title',axis= 1).drop('genres',axis= 1).drop('year',axis= 1)
+	recommendationTable_df = ((genreTable*userProfile).sum(axis=1))/(userProfile.sum())
+	recommendationTable_df = recommendationTable_df.sort_values(ascending=False)
+	return list(movies_df.loc[movies_df['movieId'].isin(recommendationTable_df.head(5).keys()).tolist()].title)
